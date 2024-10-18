22637ec2dfc91db50f13f9a78d26716c880d1520
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-18 15:16:47 +0300

    feat: start basic testing task

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,9 +1,10 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const sum = simpleCalculator({ a: 2, b: 4, action: Action.Add });
+    expect(sum).toBe(6);
   });
 
   test('should subtract two numbers', () => {
