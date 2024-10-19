59ebce7ba74758d47b718ddb37aadc5f1cb0de24
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-19 21:58:19 +0300

    init: start 05-partial-mocking task

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,12 @@
-// Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  return {
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+    unmockedFunction: jest.requireActual('./index').unmockedFunction,
+  };
 });
 
 describe('partial mocking', () => {
