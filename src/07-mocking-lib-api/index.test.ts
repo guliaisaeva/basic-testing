3e90afd9d8462434310db241e98d2cc36a65ee17
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-20 23:20:08 +0300

    fix : 07-mocking-lib-api  test task

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,13 +1,16 @@
-import axios from 'axios';
-import { throttledGetDataFromApi } from './index';
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi, THROTTLE_TIME } from './index';
 
 jest.mock('axios');
+jest.mock('lodash', () => ({
+  throttle: (fn: () => unknown) => fn,
+}));
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
-  const relativePath = '/posts/1';
-  const mockResponseData = { id: 1, title: 'Sample Post' };
-  let axiosInstance: jest.Mocked<typeof axios>;
+  let axiosInstance: jest.Mocked<AxiosInstance>;
+  const relativePath = '/blogs/1';
+  const mockResponseData = { id: 1, title: 'About Jest Testing' };
 
   beforeEach(() => {
     jest.useFakeTimers();
@@ -15,14 +18,16 @@ describe('throttledGetDataFromApi', () => {
     axiosInstance = {
       get: jest.fn().mockResolvedValue({ data: mockResponseData }),
       create: jest.fn(() => axiosInstance),
-    } as unknown as jest.Mocked<typeof axios>;
+    } as unknown as jest.Mocked<AxiosInstance>;
 
     mockedAxios.create.mockReturnValue(axiosInstance);
   });
+
   afterEach(() => {
     jest.clearAllMocks();
     jest.useRealTimers();
   });
+
   test('should create instance with provided base url', async () => {
     await throttledGetDataFromApi(relativePath);
     expect(mockedAxios.create).toHaveBeenCalledWith({
@@ -30,7 +35,14 @@ describe('throttledGetDataFromApi', () => {
     });
   });
 
-  test('should perform request to correct provided url', async () => {});
+  test('should perform request to correct provided url', async () => {
+    axiosInstance.get.mockResolvedValue({ data: mockResponseData });
+
+    const throttledCall = throttledGetDataFromApi(relativePath);
+    jest.advanceTimersByTime(THROTTLE_TIME);
+    await throttledCall;
+    expect(axiosInstance.get).toHaveBeenCalledWith(relativePath);
+  });
 
   test('should return response data', async () => {
     const responseData = await throttledGetDataFromApi(relativePath);
